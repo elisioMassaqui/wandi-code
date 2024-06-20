3c8525bd241c4e63d24d2cc27807549281e713bd
Mr.Robot != false <145590545+elisioMassaqui@users.noreply.github.com>
2024-06-20 22:06:47 +0100

    Revert "Este componente CodeEditor agora possui a capacidade de acessar diretamente o editor de código Monaco através da referência editorRef.current, e também foca no editor assim que ele é montado, proporcionando uma melhor experiência de usuário ao iniciar a edição de código."
    This reverts commit 913239c4a507ad09101b7248a753761ed7296b5d.

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,28 +1,19 @@
 import { Box } from "@chakra-ui/react"
 import { Editor } from "@monaco-editor/react"
-import { useRef, useState } from "react"
+import { useState } from "react"
 
 const CodeEditor = () => {
-
     const [value, setValue] = useState('')  // Declaração de estado usando useState. 'value' é o estado atual, 'setValue' é a função para atualizar o estado.
-    const editorRef = useRef()  // Cria uma referência mutável que será utilizada para acessar o editor.
-
-    const onMount = (editor) => {
-        editorRef.current = editor;  // Quando o editor é montado, atualiza a referência para o editor atual.
-        editor.focus();  // Foca no editor quando ele é montado.
-    }
-
     return (
-        // Área onde o código será digitado.
+        //Área onde o código será digitado.
         <Box>
             <Editor
                 height="75vh"  // Altura do editor definida como 75% da altura da viewport.
                 theme="vs-dark"  // Tema do editor (dark mode).
                 defaultLanguage="javascript"  // Linguagem de programação padrão para o editor (JavaScript neste caso).
-                defaultValue="// Seja bem vindo ao Wandi Code"  // Valor inicial do conteúdo do editor.
-                onMount={onMount}  // Callback chamado quando o editor é montado.
+                defaultValue="// some comment"  // Valor inicial do conteúdo do editor.
                 value={value}  // Propriedade 'value' do editor, que é controlada pelo estado 'value'.
-                onChange={(value) => setValue(value)}  // Função de callback chamada sempre que o conteúdo do editor é alterado, atualizando o estado 'value'.
+                onChange={(value) => setValue(value)}  // Função de callback chamada sempre que o conteúdo do editor é alterado, atualizando o estado 'value'
             />
         </Box>
     )
