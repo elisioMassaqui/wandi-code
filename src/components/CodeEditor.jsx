acf1c40b69f6a0c54dc618737e95746076a38652
Mr.Robot != false <145590545+elisioMassaqui@users.noreply.github.com>
2024-06-20 20:51:01 +0100

    O estado value controla dinamicamente o conteúdo do editor, atualizando-o conforme o usuário digita ou altera o código.

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,14 +1,21 @@
 import { Box } from "@chakra-ui/react"
 import { Editor } from "@monaco-editor/react"
+import { useState } from "react"
 
 const CodeEditor = () => {
+    const [value, setValue] = useState('')  // Declaração de estado usando useState. 'value' é o estado atual, 'setValue' é a função para atualizar o estado.
     return (
+        //Área onde o código será digitado.
         <Box>
-            <Editor height="75vh" 
-            theme="vs-dark"
-            defaultLanguage="javascript" 
-            defaultValue="// some comment" />
+            <Editor
+                height="75vh"  // Altura do editor definida como 75% da altura da viewport.
+                theme="vs-dark"  // Tema do editor (dark mode).
+                defaultLanguage="javascript"  // Linguagem de programação padrão para o editor (JavaScript neste caso).
+                defaultValue="// some comment"  // Valor inicial do conteúdo do editor.
+                value={value}  // Propriedade 'value' do editor, que é controlada pelo estado 'value'.
+                onChange={(value) => setValue(value)}  // Função de callback chamada sempre que o conteúdo do editor é alterado, atualizando o estado 'value'.
+            />
         </Box>
     )
 }
-export default CodeEditor
+export default CodeEditor  // Exporta o componente CodeEditor para uso em outros arquivos.
