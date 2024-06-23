cbe3ca6b3db714ea525ab5fce58719516dac5f40
Mr.Robot != false <145590545+elisioMassaqui@users.noreply.github.com>
2024-06-23 09:34:01 +0100

    we are back niggaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { Box, ChakraProvider, Flex, Text } from '@chakra-ui/react';
-import CodeEditor from './components/CodeEditor';
+import { Box, ChakraProvider, Flex, Text } from "@chakra-ui/react";
+import CodeEditor from "./components/CodeEditor";
 
 function App() {
   return (
     <ChakraProvider>
       <Flex minHeight="1vh" alignItems="center" justifyContent="center">
         <Box textAlign="center">
-      <Text mb={1} fontSize="6xl" fontWeight="bold" color="cyan">
-        Wandi-Code
-      </Text>
+          <Text mb={1} fontSize="6xl" fontWeight="bold" color="cyan">
+            Wandi-Code
+          </Text>
         </Box>
       </Flex>
       <Box>
-        <CodeEditor/>
+        <CodeEditor />
       </Box>
     </ChakraProvider>
   );
